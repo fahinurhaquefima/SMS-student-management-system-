f95fa6de1502dcef272efac8658695797b7b75d1
fahinurhaquefima <fahinurhaquef@gmail.com>
2024-02-28 13:08:10 +0600

    dashboard add

--- SMS.WebApp/Models/Student.cs
+++ SMS.WebApp/Models/Student.cs
@@ -1,11 +1,18 @@
-﻿namespace SMS.WebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SMS.WebApp.Models
 {
     public class Student
     {
+        [Key]
         public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter name")]
         public string Name { get; set; } = string.Empty;
+        [Required]
         public string Email { get; set; } = string.Empty;
+        [Required]
         public string Phone { get; set; } = string.Empty;
+        [Required]
         public string Address { get; set; } = string.Empty;
     }
 }
